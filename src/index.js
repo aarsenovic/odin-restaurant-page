6f1976f44ada57a38c4fd84e64dd7eec99d7ccc1
aarsenovic <acawarrior@gmail.com>
2025-10-05 02:14:36 +0200

    feat: add function that attaches correct function to matching button

--- src/index.js
+++ src/index.js
@@ -3,5 +3,34 @@ import renderHomeTab from "./homeTab"
 import { renderMenuTab } from "./menuTab";
 import renderAboutTab from "./aboutTab";
 
+const tabRenderers = {
+  home: renderHomeTab,
+  menu: renderMenuTab,
+  about: renderAboutTab,
+};
 
-renderAboutTab();
+renderHomeTab();
+
+
+function attachEventListeners() {
+  document.querySelectorAll(".nav-button").forEach(button => {
+    button.addEventListener("click", () => {
+      setActiveButton(button);  
+      const tab = button.dataset.tab;
+      const render = tabRenderers[tab];
+      if (render) render();
+    });
+  });
+}
+
+function setActiveButton(button){
+    const allButtons = document.querySelectorAll(".nav-button");
+
+    for (let i = 0; i<allButtons.length; i++) {
+        allButtons[i].classList.remove("selected");
+    }
+
+    button.classList.add("selected");
+}
+
+attachEventListeners();
